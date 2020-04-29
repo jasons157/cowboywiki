749302de140228a89513d11741be05f3d26a381f
Jason <evans072@rangers.uwp.edu>
2020-04-29 17:09:33 -0500

    initial commit

---
+++ src/main/java/personal/jasonevans/cowboywiki/CowboywikiApplication.java
@@ -0,0 +1,13 @@
+package personal.jasonevans.cowboywiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CowboywikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CowboywikiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/personal/jasonevans/cowboywiki/CowboywikiApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.jasonevans.cowboywiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CowboywikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
