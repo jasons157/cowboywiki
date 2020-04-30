ef07607e0433480d3d30ca5bfa43b988bbd5d1bf
Jason <evans072@rangers.uwp.edu>
2020-04-30 15:46:57 -0500

    Created Auth success handler and Security Config classes Mapped home ("/") to allow any user to visit Mapped login page for SecurityConfiguration Mapped access-denied page for SecurityConfiguration

---
+++ src/main/java/personal/jasonevans/cowboywiki/config/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,44 @@
+package personal.jasonevans.cowboywiki.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import personal.jasonevans.cowboywiki.entity.User;
+import personal.jasonevans.cowboywiki.service.UserService;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * This class triggers when a user is successfully authenticated.
+ * Their account will be fetched and placed into the HTTP Session.
+ * They will then be forwarded to the home page.
+ */
+public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+
+        System.out.println("Successfully authenticated");
+
+        String username = authentication.getName();
+
+        System.out.println("Current user: " + username);
+
+        //fetch user from DB
+        User theUser = userService.findByUserName(username);
+
+        //place user in session
+        HttpSession session = httpServletRequest.getSession();
+        session.setAttribute("user", theUser);
+
+        //forward to home page of "/"
+        httpServletResponse.sendRedirect(httpServletRequest.getContextPath() + "/");
+    }
+}
---
+++ src/main/java/personal/jasonevans/cowboywiki/config/SecurityConfiguration.java
@@ -0,0 +1,87 @@
+package personal.jasonevans.cowboywiki.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import personal.jasonevans.cowboywiki.service.UserService;
+
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private CustomAuthenticationSuccessHandler successHandler;
+
+    /**
+     * Sets the authentication provider as our own, with
+     * our own user service and pw encoder.
+     * @param auth authentication to set up for user
+     * @throws Exception
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+    /**
+     * Sets up permissions/denials for certain web pages based on roles
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        //TODO create login page controller
+        //TODO Create Login page
+        //TODO Create action authenticateUser in login form
+        //TODO create access denied page
+
+        http.authorizeRequests()
+                .antMatchers("/").hasRole("VISITOR")//Everyone gets visitor by default (UserServiceImpl)
+                .and()
+                .formLogin()
+                    .loginPage("/showLoginPage")
+                    .loginProcessingUrl("/authenticateUser")
+                    .successHandler(successHandler)
+                    .permitAll()//Everyone can go to the login page (or else it would defeat the point of all this)
+                .and()
+                .logout().permitAll()//Let everyone log out
+                .and()
+                .exceptionHandling().accessDeniedPage("/access-denied");
+
+
+    }
+
+    /**
+     * Bean definitions
+     */
+
+    /**
+     * Set authentication details and password encoder (uses BCrypt).
+     * Setting userservicedetails allows us to use the loadUserByUsername
+     * which includes roles.
+     * Essentially loads the roles of a user.
+     * @return Authentication provider auth
+     */
+    @Bean
+    private AuthenticationProvider authenticationProvider() {
+
+        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+        auth.setUserDetailsService(userService);
+        auth.setPasswordEncoder(passwordEncoder());
+        return auth;
+    }
+
+    //get bcrypt encoder
+    @Bean
+    private BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
